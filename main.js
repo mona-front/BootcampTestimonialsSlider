2405569e5c80065b31a8febc2f0ea21cc5ac146f
mona behkerdar <monabehkerdar@gmail.com>
2021-10-16 21:15:54 +0330

    update

--- main.js
+++ main.js
@@ -16,13 +16,6 @@ const array=[
 
 
     }
-    // {
-    //     description:" “ Monain coding for a while but never taken the jump, until now.I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”" ,
-    //     fullName:"Mona Sinclair",
-    //     job:"UX Engineer",
-    //     img:"images/image-john.jpg"
-
-    // }
 ]
 
 // items
@@ -38,8 +31,11 @@ const arrowPrev=document.querySelector(".prev")
 let counter=0;
 window.addEventListener("DOMContentLoaded",()=>{
     showPerson()
+    
 
 })
+
+
 // show person
 const showPerson=()=>{
     const Slide=array[counter]
@@ -47,16 +43,16 @@ const showPerson=()=>{
     fullName.textContent=Slide.fullName
     job.textContent=Slide.job
     image.src=Slide.img
+    
 }
 // nextSlide
 arrwoNext.addEventListener("click",()=>{
-console.log("ji")
+
     counter++
     if(counter>array.length-1){
         counter=0;
     }
     showPerson()
-
 })
 
 // prevSlide
@@ -66,6 +62,6 @@ arrowPrev.addEventListener("click",()=>{
         counter=Array.length
     }
     showPerson()
-    console.log("ji")
+    
 
 })
