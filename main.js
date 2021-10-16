b7e2cbdddaf9e406f4cff4210cd65d74ab2f7e96
mona behkerdar <monabehkerdar@gmail.com>
2021-10-16 21:30:45 +0330

    update file

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const array=[
-    // {
-    //     description:"“If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible.I now feel so confident about starting up as a professional developer. ”"
-    //      ,
+    {
+        description:"“If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible.I now feel so confident about starting up as a professional developer. ”"
+         ,
 
-    //   fullName:"John Tarkpor",
-    //   job:"Junior Front-end Developer",
-    //   img:"images/image-john.jpg"}
+      fullName:"John Tarkpor",
+      job:"Junior Front-end Developer",
+      img:"images/image-john.jpg"}
 
-    // ,
+    ,
     {
         description:" “I’vebeen interested in coding for a while but never taken the jump, until now.I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”" ,
         fullName:"Tanya Sinclair",
