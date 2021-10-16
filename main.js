df07b49fd92101b94bab22b3b5d9373797184ae3
mona behkerdar <monabehkerdar@gmail.com>
2021-10-16 21:40:02 +0330

    add image

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const array=[
         description:" “I’vebeen interested in coding for a while but never taken the jump, until now.I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”" ,
         fullName:"Tanya Sinclair",
         job:"UX Engineer",
-        img:"images/image-tanya.jpg"
+        img:"images/WHAT-IS-CSS.jpg"
     }
 ]
 
